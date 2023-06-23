698aab604f82506182df92f9131e449301e84a04
edsonibarra <edsonibarra97@gmail.com>
2023-06-23 16:02:18 -0500

    Remove tail logic

--- src/data_structures/singly_linked_list/linked_list.py
+++ src/data_structures/singly_linked_list/linked_list.py
@@ -5,9 +5,9 @@ class LinkedList:
     EMPTY_LINKED_LIST_MSG = "Linked List is Empty"
     DELETE_FROM_EMPTY_LINKED_LIST_MSG = "Could not delete from empty linked list"
     NODE_NOT_FOUND = "Node with data = {} not found in {}"
+
     def __init__(self):
         self.head = None
-        self.tail = None
 
     def append(self, data):
         """
@@ -16,14 +16,12 @@ class LinkedList:
         new_node = Node(data)
         if self.is_empty():
             self.head = new_node
-            self.tail = new_node
             return
         current_node = self.head
         while current_node.next:
             current_node = current_node.next
         current_node.next = new_node
-        self.tail = new_node
-    
+
     def print_list(self):
         """
         It prints each value in the linked list in a friendly way.
@@ -34,10 +32,10 @@ class LinkedList:
             return LinkedList.EMPTY_LINKED_LIST_MSG
         current_node = self.head
         while current_node:
-            print(f"[{current_node.data}]", end='->')
+            print(f"[{current_node.data}]", end="->")
             current_node = current_node.next
         print("None")
-    
+
     def prepend(self, data):
         """
         It adds a new Node at the beginning of the linked list.
@@ -45,7 +43,6 @@ class LinkedList:
         new_node = Node(data)
         if self.is_empty():
             self.head = new_node
-            self.tail = new_node
             return
         current_node = self.head
         new_node.next = current_node
@@ -75,31 +72,25 @@ class LinkedList:
             self.head = current_node.next
             current_node = None
             return
-        
+
         # Traverse Linked list until node containing value_to_delete is found
         while current_node and current_node.data != value_to_delete:
             prev = current_node
             current_node = current_node.next
-        
-        
-        # Check if the node to remove from linked list is the tail node
-        if current_node is self.tail:
-            # Update self.tail to the previous node
-            self.tail = prev
-            prev.next = current_node.next
-            current_node = None
-            return
+
         # If current_node exists, it means the value_to_delete node exits within linked list
-        elif current_node:
+        if current_node:
             # Update the prev.next to point to current_node.next and remove current_node form linked list
             prev.next = current_node.next
             current_node = None
             return
         # If current_node does not exists it means value_to_delete was not found in the linked list
         else:
-            message = LinkedList.NODE_NOT_FOUND.format(value_to_delete,"delete_node_by_value")
+            message = LinkedList.NODE_NOT_FOUND.format(
+                value_to_delete, "delete_node_by_value"
+            )
             print(message)
-            return message 
+            return message
 
     def delete_node_by_position(self, position_to_delete):
         if self.is_empty():
@@ -109,7 +100,7 @@ class LinkedList:
         current_node = self.head
         prev = None
         count = 0
-        
+
         # Check if the node to delete is the head node
         if position_to_delete == count:
             self.head = current_node.next
@@ -119,23 +110,17 @@ class LinkedList:
         while current_node and count != position_to_delete:
             count += 1
             current_node = current_node.next
-        
-        if current_node is self.tail:
-            # Update self.tail to the previous node
-            self.tail = prev
-            prev.next = current_node.next
-            current_node = None
-            return
+
         # If current_node exists, it means the value_to_delete node exits within linked list
-        elif current_node:
+        if current_node:
             # Update the prev.next to point to current_node.next and remove current_node form linked list
             prev.next = current_node.next
             current_node = None
             return
         # If current_node does not exists it means value_to_delete was not found in the linked list
         else:
-            message = LinkedList.NODE_NOT_FOUND.format(position_to_delete,"delete_node_by_position")
+            message = LinkedList.NODE_NOT_FOUND.format(
+                position_to_delete, "delete_node_by_position"
+            )
             print(message)
             return message
-
-    
--- tests/test_singly_linked_list.py
+++ tests/test_singly_linked_list.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.data_structures.singly_linked_list.linked_list import LinkedList
 
 
@@ -31,38 +32,3 @@ def test_linked_list_insertion_append():
     assert linked_list.head.data == 1
     linked_list.append(2)
     assert linked_list.head.next.data == 2
-    
-
-def test_linked_list_tail(populated_linked_list):
-    linked_list = populated_linked_list
-    assert linked_list.tail.data == 4
-
-def test_linked_list_tail_head_when_len_1(empty_linked_list):
-    linked_list = empty_linked_list
-
-    assert linked_list.head is None
-    assert linked_list.tail is None
-
-    linked_list.append(1)
-
-    assert linked_list.head is linked_list.tail
-    assert linked_list.head.data == linked_list.tail.data
-
-
-def test_linked_list_tail_head_are_moving_correctly(empty_linked_list):
-    linked_list = empty_linked_list
-
-    assert linked_list.head is None
-    assert linked_list.tail is None
-
-    linked_list.append(1)
-
-    assert linked_list.head is linked_list.tail
-    assert linked_list.head.data == linked_list.tail.data
-
-    linked_list.prepend(0)
-
-    assert linked_list.head.data == 0
-    assert linked_list.head is not linked_list.tail
-    assert linked_list.tail.data == 1
-    assert linked_list.head.next is linked_list.tail
